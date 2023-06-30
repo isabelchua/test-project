2011084f9666669c10378712296843b620175224
isabelchua <isabelchua86@gmail.com>
2023-06-30 11:35:19 -0700

    20230630

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,38 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Hawaii", "London", "Tokyo"];
+  let items = ["New York", "San Francisco", "Hawaii", "London", "Tokyo"];
+  //   let selectedIndex = 0;
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //   const handleClick = (event: MouseEvent) => console.log(event);
+
+  //   items = [];
+
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No Items found.</p> : null;
+  //   };
 
   return (
     <>
       <h1>List</h1>
-
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No Items Found. </p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
-        {/* <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li> */}
       </ul>
     </>
   );
