9b16aec2040351f7ef63bf8e5a58eb3599872e5e
isabelchua <isabelchua86@gmail.com>
2023-07-06 22:23:52 -0700

    20230706

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
+import Alert from "./Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Hawaii", "London", "Tokyo"];
+  //   let items = ["New York", "San Francisco", "Hawaii", "London", "Tokyo"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  //   const handleSelectItem = (item: string) => {
+  //     console.log(item);
+  //   };
+  //  {/* <ListGroup
+  //      items={items}
+  //      heading="Cities"
+  //      onSelectItem={handleSelectItem}
+  //    /> */}
 
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
-  );
+  return <Alert>Hello World</Alert>;
 }
 
 export default App;
